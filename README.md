885803810d951379f1b379fa81d2187d35f73b6b
Alexandru Andrasescu <andrasescu_alexandru@yahoo.com>
2016-06-15 14:24:24 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# demo quiz app for learning javascript
---
+++ gulpfile.js
@@ -0,0 +1,86 @@
+'use strict';
+
+let gulp = require('gulp'),
+    del = require('del'),
+    browserSync = require('browser-sync').create(),
+    babel = require('gulp-babel'),
+    concat = require('gulp-concat'),
+    sourcemaps = require('gulp-sourcemaps'),
+    sass = require('gulp-sass');
+
+const BASE_PATH = './app';
+
+// process JS files and return the stream
+gulp.task('jsSrc', ['clean'], () => {
+    // return the stream
+    return gulp
+        // read source files
+        .src([
+            `${BASE_PATH}/src/js/*.js`,
+            `${BASE_PATH}/src/js/app.js`
+        ])
+        // start sourcemaps
+        .pipe(sourcemaps.init())
+        // transpile using Babel to ES5
+        .pipe(babel({
+            presets: ['es2015']
+        }).on('error', handleError))
+        // concatenate all source files into a single file
+        .pipe(concat('bundle.js'))
+        // output sourcemaps
+        .pipe(sourcemaps.write('maps/'))
+        // output stream, containing
+        .pipe(gulp.dest(`${BASE_PATH}/dist`));
+});
+
+// process JS vendor files
+gulp.task('jsVendor', ['clean'], () => {
+    return gulp
+        .src([
+            `${BASE_PATH}/vendor/jquery*.js`,
+            `${BASE_PATH}/vendor/*.js`,
+        ])
+        .pipe(concat('vendor.js'))
+        .pipe(gulp.dest(`${BASE_PATH}/dist`));
+});
+
+// process SASS files and return the stream
+gulp.task('sass', function () {
+    return gulp.src(`${BASE_PATH}/src/sass/*.scss`)
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest(`${BASE_PATH}/src/css`));
+});
+
+// create a task that ensures the `js` task is complete
+// before reloading browsers
+gulp.task('jsWatch', ['jsVendor', 'jsSrc'], () => {
+    return browserSync.reload();
+});
+
+gulp.task('clean', () => {
+    return del([`${BASE_PATH}/dist`]);
+});
+
+// use default task to compile JS files
+// then launch Browsersync and watch files
+gulp.task('default', ['jsVendor', 'jsSrc'], () => {
+
+    // Serve files from the root of our app
+    browserSync.init({
+        server: {
+            baseDir: BASE_PATH
+        }
+    });
+
+    // when HTML files change, only reload the browsers
+    gulp.watch(`${BASE_PATH}/*.html`).on('change', browserSync.reload);
+    // when JS files change, compile and then reload browsers
+    gulp.watch(`${BASE_PATH}/src/js/*.js`, ['jsWatch']);
+    // when SASS files change compile and then reload browsers
+    gulp.watch(`${BASE_PATH}/src/sass/*.scss`, ['sass']);
+});
+
+function handleError(err) {
+    console.log(err.toString());
+    this.emit('end');
+}
